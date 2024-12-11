5f82cc16670a387f3d30277d2d059d369225f6c2
Tunao2den <tuna.ozden.810@gmail.com>
2024-12-11 17:37:22 +0300

    user service v2 created

---
+++ users-service-v2/src/main/java/com/tuna/usersservice/UsersServiceV2Application.java
@@ -0,0 +1,13 @@
+package com.tuna.usersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersServiceV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceV2Application.class, args);
+    }
+
+}
---
+++ users-service-v2/src/test/java/com/tuna/usersservice/UsersServiceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuna.usersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
