e7cb2df7acfcc1cda02a1113157a803d19bdc6dd
Tunao2den <tuna.ozden.810@gmail.com>
2024-12-14 18:29:21 +0300

    tables and entitys created

---
+++ post-service-v2/src/main/java/com/tuna/postservice/model/entity/MasterPostCategory.java
@@ -0,0 +1,26 @@
+package com.tuna.postservice.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "master_post_category")
+@Getter
+@Setter
+@NoArgsConstructor
+public class MasterPostCategory {
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Integer id;
+    @Column(name = "category_name")
+    private String category;
+    @OneToMany(mappedBy = "masterPostCategory", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<MasterPost> masterPosts;
+}
