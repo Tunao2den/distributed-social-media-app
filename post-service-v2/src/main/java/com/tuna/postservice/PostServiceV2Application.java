01e796b5e26e1bf642ccfa73ec4bd8bb5650a3c6
Tunao2den <tuna.ozden.810@gmail.com>
2024-12-14 16:38:15 +0300

    post service initialized

---
+++ post-service-v2/src/main/java/com/tuna/postservice/PostServiceV2Application.java
@@ -0,0 +1,13 @@
+package com.tuna.postservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostServiceV2Application.class, args);
+    }
+
+}
---
+++ post-service-v2/src/test/java/com/tuna/postservice/PostServiceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuna.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
