cbc6c15a05d02c97af5f34c58c37084982cc7f6a
Tunao2den <tuna.ozden.810@gmail.com>
2024-12-28 20:12:16 +0300

    api gateway v2 initialized

---
+++ api-gateway-v2/src/test/java/com/tuna/apigateway/ApiGatewayV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuna.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
